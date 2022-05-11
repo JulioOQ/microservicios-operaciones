d1a0c3da414b140bf62b03bb951c62cc5d34503a
Julio Vladimir Ore Quispe <jorequis@emeal.nttdata.com>
2022-05-11 15:47:56 -0500

    lb

--- src/main/java/com/jvoq/microservicios/operaciones/app/controllers/TransactionController.java
+++ src/main/java/com/jvoq/microservicios/operaciones/app/controllers/TransactionController.java
@@ -25,7 +25,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/transactions")
+//@RequestMapping("/transactions")
 public class TransactionController {
 
 	@Autowired
